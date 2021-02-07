16a53ffbd01a0f2beb3508ff76c42dffb9df50f6
Megapixel-code <77630002+Megapixel-code@users.noreply.github.com>
2021-02-07 22:40:25 +0100

    added textures

--- 3d render.py
+++ 3d render.py
@@ -8,6 +8,36 @@ window = pygame.display.set_mode((990, 990))
 carPos = (150, 150)
 carAngle = 180
 
+# ====================================================== TEXTURES ======================================================
+
+gray = (49, 47, 47)
+white = (246, 232, 234)
+
+wall = [[gray, gray, gray, gray, gray, gray, gray, gray, gray, gray],
+        [gray, gray, white, white, white, white, white, white, gray, gray],
+        [gray, white, gray, white, white, white, white, gray, white, gray],
+        [gray, white, white, gray, white, white, gray, white, white, gray],
+        [gray, white, white, white, gray, gray, white, white, white, gray],
+        [gray, white, white, white, gray, gray, white, white, white, gray],
+        [gray, white, white, gray, white, white, gray, white, white, gray],
+        [gray, white, gray, white, white, white, white, gray, white, gray],
+        [gray, gray, white, white, white, white, white, white, gray, gray],
+        [gray, gray, gray, gray, gray, gray, gray, gray, gray, gray]]
+
+wall_2 = [[gray, gray, gray, gray, gray, gray, gray, gray, gray, gray],
+          [gray, white, gray, white, gray, gray, white, gray, white, gray],
+          [gray, white, gray, white, gray, gray, white, gray, white, gray],
+          [gray, white, gray, white, gray, gray, white, gray, white, gray],
+          [gray, white, gray, white, gray, gray, white, gray, white, gray],
+          [gray, white, gray, white, gray, gray, white, gray, white, gray],
+          [gray, white, gray, white, gray, gray, white, gray, white, gray],
+          [gray, white, gray, white, gray, gray, white, gray, white, gray],
+          [gray, white, gray, white, gray, gray, white, gray, white, gray],
+          [gray, gray, gray, gray, gray, gray, gray, gray, gray, gray]]
+
+
+# ====================================================== TEXTURES ======================================================
+
 
 class Ray:
     def __init__(self, x, y, ray_angle):
@@ -18,16 +48,64 @@ class Ray:
         self.yAdd = - math.cos(math.radians(ray_angle))
 
 
+class Mob:
+    def __init__(self, x, y, lvl):
+        self.health = lvl
+        self.x = x
+        self.y = y
+        self.lvl = lvl
+
+    def move(self):
+        pass
+
+    def give_angle(self):
+        relative_x = self.x - carPos[0]
+        relative_y = self.y - carPos[1]
+        if relative_y == 0 and relative_x > 0:
+            return 90
+        if relative_y == 0 and relative_x < 0:
+            return 270
+        if relative_y == 0:
+            return None
+        mob_angle = math.degrees(math.atan(relative_x / relative_y))
+        mob_angle = -mob_angle
+        if relative_x >= 0 and relative_y <= 0:
+            return mob_angle
+        if relative_y > 0:
+            return mob_angle + 180
+        if relative_x <= 0 and relative_y <= 0:
+            return mob_angle + 360
+
+
+# ______________ TWEAKING TEXTURES ______________
+
+
+def normalize_textures(my_list):
+    all_textures = []
+    for e in my_list:
+        temp = []
+        for i in range(10):
+            temp_2 = []
+            for j in range(10):
+                temp_2.append(e[j][-(i + 1)])
+            temp.append(temp_2)
+        all_textures.append(temp)
+    return all_textures
+
+
+# ______________ MAP MODIFICATIONS ______________
+
+
 def validate_map(new_list):
-    new_list[1][1] = 2
+    new_list[1][1] = -1
     modified = True
     while modified:
         modified = False
         for x in range(len(new_list) - 2):
             for y in range(len(new_list) - 2):
-                if new_list[y + 1][x + 1] == 0 and (new_list[y][x + 1] == 2 or new_list[y + 1][x] == 2 or
-                                                    new_list[y + 2][x + 1] == 2 or new_list[y + 1][x + 2] == 2):
-                    new_list[y + 1][x + 1] = 2
+                if new_list[y + 1][x + 1] == 0 and (new_list[y][x + 1] == -1 or new_list[y + 1][x] == -1 or
+                                                    new_list[y + 2][x + 1] == -1 or new_list[y + 1][x + 2] == -1):
+                    new_list[y + 1][x + 1] = -1
                     modified = True
         if not modified:
             for i in range(len(new_list)):
@@ -36,16 +114,16 @@ def validate_map(new_list):
                         return False
             for i in range(len(new_list) - 2):
                 for k in range(len(new_list) - 2):
-                    if new_list[i + 1][k + 1] == 1 and new_list[i + 2][k + 2] == 1 and new_list[i + 2][k + 1] == 2 and \
-                            new_list[i + 1][k + 2] == 2:
+                    if new_list[i + 1][k + 1] != -1 and new_list[i + 2][k + 2] != -1 and \
+                            new_list[i + 2][k + 1] == -1 and new_list[i + 1][k + 2] == -1:
                         return False
-                    if new_list[i + 1][k + 1] == 2 and new_list[i + 2][k + 2] == 2 and new_list[i + 2][k + 1] == 1 and \
-                            new_list[i + 1][k + 2] == 1:
+                    if new_list[i + 1][k + 1] == -1 and new_list[i + 2][k + 2] == -1 and \
+                            new_list[i + 2][k + 1] != -1 and new_list[i + 1][k + 2] != -1:
                         return False
 
     for x in range(len(new_list)):
         for y in range(len(new_list)):
-            if new_list[x][y] == 2:
+            if new_list[x][y] == -1:
                 new_list[x][y] = 0
     return True
 
@@ -67,10 +145,13 @@ def create_map():
         for y in range(8):
             for x in range(10):
                 if my_map[y + 1][x] == 0 and random.randint(0, 1) == 0:
-                    my_map[y + 1][x] = 1
+                    my_map[y + 1][x] = random.randint(1, len(textures))
         my_map[1][1] = 0
         cont = validate_map(my_map.copy())
     return my_map
+
+
+# ______________ RENDERING ______________
 
 
 def relative_dist(pos):
@@ -79,27 +160,46 @@ def relative_dist(pos):
     x -= int(x / 100) * 100
     y -= int(y / 100) * 100
     if x > y:
-        return x
+        return x, True
     else:
-        return y
+        return y, False
+
+
+def give_wall(pos, mapList):
+    for i in range(3):
+        for j in range(3):
+            x = mapList[int((pos[1] + (i - 1)) / 100)][int((pos[0] + (j - 1)) / 100)]
+            if x != 0:
+                return x
 
 
 def display(ray, n, mapList):
     wall_pos = dist(ray, mapList)
     relative_pos = relative_dist(wall_pos)
     my_dist = math.sqrt((wall_pos[0] - int(carPos[0])) ** 2 + (wall_pos[1] - int(carPos[1])) ** 2)
-    col = my_dist / 8
-    if col > 100:
-        col = 100
     height = ((990 / (my_dist + 1)) * 100 - math.sin(math.radians(n * (180 / 200))) * 20)
 
-    if 0 <= relative_pos < 25 / 2 or 25 / 2 + 25 < relative_pos < 25 / 2 + 50 or 25 / 2 + 75 < relative_pos <= 100:
-        col = int(col / 10)
-        pygame.draw.rect(window, [14 - col, 32 - col, 43 - col / 1.05],
-                         [n * 5 - 2.5, (990 - height) / 2, 5, height])
+    color_cor = my_dist / 200
+    if color_cor < 1:
+        color_cor = 1
+
+    my_wall = textures[give_wall(wall_pos, mapList) - 1]
+
+    if 90 > ray.angle or ray.angle > 270 and relative_pos[1]:
+        for i in range(10):
+            col = my_wall[-(int(relative_pos[0] / 10) + 1)][i]
+            pygame.draw.rect(window, [int(col[0] / color_cor), int(col[1] / color_cor), int(col[2] / color_cor)],
+                             [n * 5 - 2.5, (1000 - height) / 2 + height / 10 * i, 5, height / 10 + 1])
+    elif 0 < ray.angle < 180 and relative_pos[1] is False:
+        for i in range(10):
+            col = my_wall[-(int(relative_pos[0] / 10) + 1)][i]
+            pygame.draw.rect(window, [int(col[0] / color_cor), int(col[1] / color_cor), int(col[2] / color_cor)],
+                             [n * 5 - 2.5, (1000 - height) / 2 + height / 10 * i, 5, height / 10 + 1])
     else:
-        pygame.draw.rect(window, [180 - col, 184 - col, 171 - col / 1.05],
-                         [n * 5 - 2.5, (990 - height) / 2, 5, height])
+        for i in range(10):
+            col = my_wall[int(relative_pos[0] / 10)][i]
+            pygame.draw.rect(window, [int(col[0] / color_cor), int(col[1] / color_cor), int(col[2] / color_cor)],
+                             [n * 5 - 2.5, (1000 - height) / 2 + height / 10 * i, 5, height / 10 + 1])
 
 
 def in_wall(mapList, posX, posY, ray):
@@ -107,11 +207,11 @@ def in_wall(mapList, posX, posY, ray):
         return False
     x = int(posX / 100)
     y = int(posY / 100)
-    if mapList[y][x] == 1:
+    if mapList[y][x] != 0:
         return True
     x = int((posX + ray.xAdd) / 100)
     y = int((posY + ray.yAdd) / 100)
-    if mapList[y][x] == 1:
+    if mapList[y][x] != 0:
         return True
     return False
 
@@ -158,26 +258,39 @@ def dist(ray, mapList):
     return best_x
 
 
-world_map = create_map()
-while game:
-    actualTime = time.process_time_ns()
-    window.fill((119, 98, 88))
-    pygame.draw.rect(window, (19, 111, 99), (0, 0, 1000, 500))
-
-    allRays = []
+def render():
+    all_rays = []
     for j in range(200):
         angle = carAngle + j / 4 - 25
         if angle < 0:
             angle += 360
         elif angle >= 360:
             angle -= 360
-        allRays.append(Ray(int(carPos[0]), int(carPos[1]), angle))
-    for j in range(len(allRays)):
-        display(allRays[j], j, world_map)
+        all_rays.append(Ray(int(carPos[0]), int(carPos[1]), angle))
+    for j in range(len(all_rays)):
+        display(all_rays[j], j, world_map)
+
+
+textures = normalize_textures([wall, wall_2])
+world_map = create_map()
+while game:
+    # ===================================== INITIALIZATION OF VARIABLE AND SCREEN =====================================
+
+    actualTime = time.process_time_ns()
+    window.fill((119, 98, 88))
+    pygame.draw.rect(window, (19, 111, 99), (0, 0, 1000, 500))
+
+    # ============================================ RENDERING ALL THE WINDOW ============================================
+
+    render()
+
+    # =================================== WAITING FOR THE END OF THE FRAME (~30 FPS) ===================================
 
     if time.process_time_ns() < actualTime + 30000000:
         time.sleep(0.01)
     pygame.display.update()
+
+    # ========================================== KEYS DETECTIONS AND MOVEMENT ==========================================
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
